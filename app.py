3e0d8c996cfe7b4dca618b890cb7823315d6b0d9
javier vera <jxvera@gmail.com>
2019-03-09 18:16:33 -0300

    mensaje

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def map_esp(number):
 )
 def update_output_div(input_value):
     if int(input_value) < 1 or int(input_value) > 9999:
-        return '"{}" no pertenece al rango entre 1 y 9999'.format(int(input_value))
+        return 'Aún no podemos traductir números en ese rango :('
     else:
         return 'En mapudungun, el número "{}" se dice'.format(int(input_value))+map_esp(int(input_value))
 
